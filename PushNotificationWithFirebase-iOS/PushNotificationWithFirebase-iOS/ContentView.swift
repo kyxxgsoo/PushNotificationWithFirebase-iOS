c0d7235c07d0fa9516a9bdf302d21430f6ae39a8
KyungsooLee <jmtkd9196@gmail.com>
2023-11-08 03:28:45 +0900

    chore: 프로젝트 셋팅

---
+++ PushNotificationWithFirebase-iOS/PushNotificationWithFirebase-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PushNotificationWithFirebase-iOS
+//
+//  Created by Kyungsoo Lee on 11/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PushNotificationWithFirebase-iOS/PushNotificationWithFirebase-iOS/PushNotificationWithFirebase_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationWithFirebase_iOSApp.swift
+//  PushNotificationWithFirebase-iOS
+//
+//  Created by Kyungsoo Lee on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationWithFirebase_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
